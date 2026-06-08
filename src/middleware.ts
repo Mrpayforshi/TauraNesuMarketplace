e841630dd16a6fba46d6873a965b3393c54358eb
Mrpayforshi <tadiwamandizha3@gmail.com>
2026-06-08 19:53:51 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-
+import { createServerSupabaseClient } from './lib/supabase';
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
