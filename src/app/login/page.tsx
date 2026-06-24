fa1110a8121787c03f3439a5d4327764380cf365
Mrpayforshi <tadiwamandizha3@gmail.com>
2026-06-24 04:25:41 +0200

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -185,12 +185,6 @@ export default function LoginPage() {
                   </svg>
                   Dealer Portal
                 </Link>
-                <Link href="/admin" className={styles.altBtn}>
-                  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/>
-                  </svg>
-                  Admin Panel
-                </Link>
               </div>
 
             </div>
